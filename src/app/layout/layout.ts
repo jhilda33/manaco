be6da763513349382cf5c98e8bd2ced1669acb5b
Jhilda Achacollo <jhildaaliz@gmail.com>
2025-10-13 22:52:48 -0400

    feat (nav + footer:agregar navegacion y pie de pagina)

--- src/app/layout/layout.ts
+++ src/app/layout/layout.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "../../../node_modules/@angular/router/router_module.d";
 
 @Component({
   selector: 'app-layout',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css'
 })
